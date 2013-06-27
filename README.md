bb178466b89b79a1d785adb44d165be7b23ddd36
Brice TENCE <brice@tence>
2013-06-27 22:40:25 +0200

    First version : seems to work fine for basic usage

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-jSass
+What is jsass ?
+==============================
+
+This project is a plugin for [Jelix](http://jelix.org) PHP framework. It allows you to use easily [Sass](http://sass-lang.com/) dynamic stylesheet language in Jelix.
+
+This is a plugin of CSSprepro which is itself an htmlresponse plugin for Jelix.
+
+
+
+Installation
+============
+
+Under Jelix default configuration, create (if missing) a "CSSprepro" directory in your project's "plugins" directory.
+Clone this repository in that directory with :
+
+    git clone --recursive git@github.com:brice-t/jSass.git
+
+
+Note that you should have your app plugin directory in your modulesPath (defaultconfig.ini.php or entry point's config.ini.php) to get it working.
+The value should be at least :
+
+    modulesPath="app:modules/"
+
+You need an up and running installation of Sass on command line to get it working (e.g. on a Debian-based distro, ''apt-get install ruby-sass'' will do the trick).
+
+
+
+Usage
 =====
 
-Jelix sub-plugin (CSSprepro plugin) to get Sass files compiled
+When including a CSS file (e.g. with addCSSLink()) you should set 'sass'=>true or 'scss'=>true as a param.
+
+Another way of having a file compiled with Sass is including as file with .scss or .sass extension. You can set expected extensions in the comma-separated value of _CSSprepro\_jSass\_extensions_ under the _jResponseHtml_ section.
+
+E.g. in your response :
+
+`$this->addCSSLink($gJConfig->urlengine['basePath'].'themes/'.$gJConfig->theme.'/Css/style.sass');`
+
+or
+
+`$this->addCSSLink($gJConfig->urlengine['basePath'].'themes/'.$gJConfig->theme.'/Css/style.sassFile', array( 'sass' => true ));`
+
+
+Your config file must activate jSass plugin :
+
+    [jResponseHtml]
+    plugins=jSass
+
+N.B. : the directories containing Sass files should be writable by your web server ! Indeed, compiled files will be written in that very same directory so that relative urls go on working ...
+
+
+
+
+Config
+======
+
+You can configure jSass's behviour regarding compilation:
+
+    [jResponseHtml]
+    ;...
+    ; always|onchange|once
+    CSSprepro_jSass_compile=always
+
+If CSSprepro\_jSass\_compile's value is not valid or empty, its default value is onchange.
+
+* always : compile Sass file on all requests
+* onchange : compile Sass file only if it has changed
+* once : compile Sass file once and never compile it again (until compiled file is removed)
+
---
+++ jSass.CSSprepro.php
@@ -0,0 +1,104 @@
+<?php
+/**
+* @package     
+* @subpackage  
+* @author      Brice Tencé
+* @copyright   2012 Brice Tencé
+* @link        
+* @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+/**
+* plugin for jResponseHTML, which processes Sass files
+*/
+
+define( 'COMPILE_TYPE_SASS', 'sass');
+define( 'COMPILE_TYPE_SCSS', 'scss');
+
+class jSassCSSpreproPlugin implements ICSSpreproPlugin {
+
+    private $sassExtensions = array('sass', 'scss');
+    private $sassPath = '/usr/bin/sass';
+    private $compileType = null;
+    private $loadPaths = array();
+    private $sassStyle = 'nested'; // nested (default), compact, compressed, or expanded
+
+    public function __construct( CSSpreproHTMLResponsePlugin $CSSpreproInstance ) {
+
+        $gJConfig = jApp::config();
+
+        if( isset($gJConfig->jResponseHtml['CSSprepro_jSass_extensions']) ) {
+            $extString = $gJConfig->jResponseHtml['CSSprepro_jSass_extensions'];
+            $this->sassExtensions = preg_split( '/ *, */', trim($extString) );
+        }
+
+        if( isset($gJConfig->jResponseHtml['CSSprepro_jSass_sasspath']) ) {
+            $this->sassPath = $gJConfig->jResponseHtml['CSSprepro_jSass_sasspath'];
+        }
+
+        if( isset($gJConfig->jResponseHtml['CSSprepro_jSass_style']) ) {
+            $this->sassStyle = $gJConfig->jResponseHtml['CSSprepro_jSass_style'];
+        }
+
+        if( isset($gJConfig->jResponseHtml['CSSprepro_jSass_loadpaths']) ) {
+            $this->loadPaths = $gJConfig->jResponseHtml['CSSprepro_jSass_loadpaths'];
+        }
+    }
+
+
+    public function handles( $inputCSSLinkUrl, $CSSLinkParams ) {
+        $this->compileType = ( isset($CSSLinkParams['sass']) && $CSSLinkParams['sass'] ?
+            COMPILE_TYPE_SASS :
+                isset($CSSLinkParams['scss']) && $CSSLinkParams['scss'] ?
+                COMPILE_TYPE_SCSS : null );
+
+        if( in_array( pathinfo($inputCSSLinkUrl, PATHINFO_EXTENSION), $this->sassExtensions ) ||
+            (isset($CSSLinkParams['sass']) && $CSSLinkParams['sass']) ) {
+                return true;
+            }
+    }
+
+    public function compile( $filePath, $outputPath ) {
+
+        $sassProcessArgs = array();
+
+        $sassProcessArgs[] = '--load-path';
+        $sassProcessArgs[] = dirname( $filePath );
+
+        if( $this->compileType == COMPILE_TYPE_SCSS ||
+             ($this->compileType === null && 'scss' == pathinfo($filePath, PATHINFO_EXTENSION)) ) {
+            $sassProcessArgs[] = '--scss';
+        }
+
+        $sassProcessArgs[] = '--style';
+        $sassProcessArgs[] = $this->sassStyle;
+
+        foreach ($this->loadPaths as $loadPath) {
+            $sassProcessArgs[] = '--load-path';
+            $sassProcessArgs[] = $loadPath;
+        }
+
+        // input
+        $sassProcessArgs[] = $filePath;
+
+
+        $sassProcessArgs = array_map( 'escapeshellarg', $sassProcessArgs );
+        $sassProcessCmd = $this->sassPath . ' ' . implode(' ', $sassProcessArgs);
+
+        exec( $sassProcessCmd, $sassProcessOutput, $code );
+
+        if( $code !== 0 ) {
+            trigger_error( "Sass error (returned $code) for '$filePath' : " . implode("\n", $sassProcessOutput), E_USER_ERROR );
+        }
+
+        file_put_contents( $outputPath, implode("\n", $sassProcessOutput) );
+    }
+
+
+    public function cleanCSSLinkParams( & $CSSLinkParams ) {
+        unset($CSSLinkParams['sass']);
+    }
+
+}
+
+
